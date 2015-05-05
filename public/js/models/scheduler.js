52a8243a371a99a557753bd3d979c9786fdee567
Diego Fu <diegofu714@hotmail.com>
2015-05-05 09:39:32 +0100

    Calculating the min time of a scheduler

--- public/js/models/scheduler.js
+++ public/js/models/scheduler.js
@@ -6,9 +6,20 @@ define(['underscore', 'backbone', 'moment'], function(_, Backbone, moment) {
         urlRoot: '/calendars/all',
         minAvailability: function() {
             console.log(this);
-            return _.min(this.get('DayOfWeek'), function(availability) {
-                return moment(availability.startTime, 'H:mm').unix();
-            });
+            var min = Number.MAX_VALUE;
+            _.each(this.get('DayOfWeeks'), function(day) {
+                var temp_min = _.min(day.Availabilities, function(availability) {
+                    console.log(moment(availability.startTime, 'H:mm').unix());
+                    return moment(availability.startTime, 'H:mm').unix();
+                });
+
+                console.log(temp_min);
+                if(min > temp_min) {
+                    min = temp_min;
+                }
+            });1
+
+            return min;
         }
     });
 
