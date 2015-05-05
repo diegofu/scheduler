45d9bc0eae803420c29256cd9249715d0f7debbe
Diego Fu <diegofu714@hotmail.com>
2015-05-05 08:55:08 +0100

    Calculating the min time of a scheduler

--- public/js/models/scheduler.js
+++ public/js/models/scheduler.js
@@ -1,9 +1,15 @@
-define(['underscore', 'backbone'], function(_, Backbone) {
+define(['underscore', 'backbone', 'moment'], function(_, Backbone, moment) {
     var Scheduler = Backbone.Model.extend({
         initialize: function(options) {
             this.options = options;
         },
-        urlRoot: '/calendars/all'
+        urlRoot: '/calendars/all',
+        minAvailability: function() {
+            console.log(this);
+            return _.min(this.get('DayOfWeek'), function(availability) {
+                return moment(availability.startTime, 'H:mm').unix();
+            });
+        }
     });
 
     return Scheduler;
